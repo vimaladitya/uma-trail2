fe4335be1f4bfd5d6b2da4674219f0e9634a94d5
vimaladitya <vimalvas96@gmail.com>
2020-10-18 09:10:15 +0530

    added token to s8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -127,7 +127,7 @@ export class AppComponent implements OnInit {
       },
       //"to":"d_lfdeZXSum4hYYVe32toF:APA91bErSMEyWx6R-e78eVyvL2Dvm9SAfMiQ0n4g2d7NiAm29qUqVqOsMC8PLT54EjNtREQ8rBlQM9V2Ff1ePhlAPyKRtZQWbp4tRON8PGR4vTYjN28J8-kxhlUg31R5sjhOoC22f8zt"
       //"to":"dcqOW1FpSamYptzSyt-4BC:APA91bGerxgWqnxrdDrTQq6LljEmgvBK5lh7aLglGg_o_0E2hGaLAJ129WyP7B8VPPgaAhOb69hl9E_NnJsYHA7dImXAHl31C8-TWJCAIO8AM_KSy5q0GSo8HONQ_OdIBZxMjQgPAYMW"// new MI
-      "to":"cmmgMWV8R66aoqX5ko_ES2:APA91bHX1J9nbJ42vKgvqS7xff32q5cMR5hN245hTEyk6zTG3cvIgwr5BGimamnKK1xRvns-lfI4-G7Wgdv9lKOsNYWU5_To8eVferoXGRJytfxBUjf_Tdte1exblLsqvEC_cCLcJTLZ"
+      "to":"cPz0tvyqRU2y46B1ZnCsuq:APA91bGIMnlCkoc8xzEemIEwdANRNFWAE8CXn0zAdaD-EdLd3_MzAPNId7VvYaJt2Hu94QDNS1-PuyLa08KbQ221jg3m5ouBaXPuBzweH1apza9GMJsna-v6Y93k0F3MlTYFoi1kBeAV"
     }, httpOptions).subscribe(posts => {
 
       console.log(posts);
